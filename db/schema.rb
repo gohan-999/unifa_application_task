8de95290d1250ebfb62a098aa2a4f995ff220b24
井上裕之 <omega999elt@gmail.com>
2022-05-19 01:15:20 +0900

    add photoテーブル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_15_095814) do
+ActiveRecord::Schema.define(version: 2022_05_16_164249) do
+
+  create_table "photos", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name"
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_photos_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "login_id"
@@ -20,4 +29,5 @@ ActiveRecord::Schema.define(version: 2022_05_15_095814) do
     t.index ["login_id"], name: "index_users_on_login_id", unique: true
   end
 
+  add_foreign_key "photos", "users"
 end
